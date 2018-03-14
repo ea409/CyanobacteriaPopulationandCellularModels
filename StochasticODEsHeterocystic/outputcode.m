b05b1f85d72ec74537f268537981ad68a349bad8
Ediem Aljibury <ediemaljibury@user-33-235.vpn.net.private.cam.ac.uk>
2018-03-14 17:36:33 +0000

    removing nonsense

--- StochasticODEsHeterocystic/outputcode.m
+++
@@ -1,9 +0,0 @@
-% This writes the outputs of rgf to files
-
-csvwrite("time35.csv", dvc);
-csvwrite("length35.csv", lngtt);
-csvwrite("prhcy35.cvs", prhcy);
-csvwrite("fNlev35.csv", fNlev);
-csvwrite("fClev35.csv", fClev);
-csvwrite("heterocysts35.csv", hcy);
-csvwrite("M35.csv", M);
